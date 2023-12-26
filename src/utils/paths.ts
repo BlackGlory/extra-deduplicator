77bcd54ac62bd98a88befd3a1bc882ddc1c8fbce
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-12-26 16:34:08 +0800

    fix: app root when hallu is compiled

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -7,7 +7,7 @@ export function getAppRoot(): string {
   if (isCompiled()) {
     // 在脚本经过编译的情况下, `Deno.execPath()`返回编译得到的可执行文件的路径.
     // 当脚本未经过编译的情况下, `Deno.execPath`为Deno可执行文件的路径, 故无法使用.
-    return Deno.execPath()
+    return path.dirname(Deno.execPath())
   } else {
     // 不在此使用更方便的`Deno.mainModule`是因为调用此函数时, 入口模块不一定是`/main.ts`.
     return path.dirname(path.join(path.fromFileUrl(import.meta.url), '../..'))

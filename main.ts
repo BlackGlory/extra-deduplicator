8775966a1799da7ef831e595872b15fb5e202991
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-01-10 00:51:20 +0800

    style: format

--- main.ts
+++ main.ts
@@ -3,15 +3,22 @@ import halluStartup from '@scripts/hallu-startup.ts'
 import subscribeRSS from '@scripts/subscribe-rss.ts'
 import watchPageChanges from '@scripts/watch-page-changes.ts'
 
-start(halluStartup(), {
-  once: true
-, ignoreInitialCommit: false
-, ignoreStartupCommit: false
-})
+start(
+  halluStartup()
+, {
+    once: true
+  , ignoreInitialCommit: false
+  , ignoreStartupCommit: false
+  }
+)
 
-start(subscribeRSS('https://news.ycombinator.com/rss'))
+start(
+  subscribeRSS('https://news.ycombinator.com/rss')
+)
 
-start(watchPageChanges({
-  name: 'IP Address'
-, url: 'https://icanhazip.com/'
-}))
+start(
+  watchPageChanges({
+    name: 'IP Address'
+  , url: 'https://icanhazip.com/'
+  })
+)

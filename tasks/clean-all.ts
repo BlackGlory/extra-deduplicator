3f9b139cd36fa03cadf8cb69275c5f3e69d2bb80
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-01-17 13:31:33 +0800

    chore: remove all tasks

--- tasks/clean-all.ts
+++
@@ -1,4 +0,0 @@
-import { remove } from 'npm:extra-filesystem@^0.5.1'
-import { getStoragesRoot } from '@utils/paths.ts'
-
-await remove(getStoragesRoot())
--- tasks/clean.ts
+++
@@ -1,9 +0,0 @@
-import { each } from 'https://esm.sh/extra-promise@6.0.8'
-import { uniq } from 'https://esm.sh/iterable-operator@4.0.6'
-import { Storage } from '@utils/storage.ts'
-
-const ids = Deno.args
-await each(uniq(ids), async id => {
-  const storage = await Storage.create(id)
-  await storage.remove()
-})

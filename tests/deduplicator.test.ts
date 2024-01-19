58a1ac0569913094a59bcb4d549a2cc47e5c1caf
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-01-19 23:16:23 +0800

    test: add hash related cases

--- tests/deduplicator.test.ts
+++ tests/deduplicator.test.ts
@@ -1,6 +1,7 @@
 import { describe, it } from 'https://deno.land/std@0.208.0/testing/bdd.ts'
 import { expect } from 'https://deno.land/std@0.212.0/expect/mod.ts'
 import { Deduplicator } from '@src/deduplicator.ts'
+import { toString } from 'https://esm.sh/@blackglory/prelude@0.3.4'
 
 describe('Deduplicator', () => {
   it('create, close', async () => {
@@ -9,40 +10,92 @@ describe('Deduplicator', () => {
     deduplicator.close()
   })
 
-  it('add', async () => {
-    const deduplicator = await Deduplicator.create<string>()
+  describe('add', () => {
+    it('default', async () => {
+      const deduplicator = await Deduplicator.create<string>()
 
-    try {
       await deduplicator.add([])
       await deduplicator.add(['foo', 'bar'])
-    } finally {
-      deduplicator.close()
-    }
+
+      expect(await deduplicator.diff(['foo', 'bar'])).toStrictEqual([])
+    })
+
+    it('with custom hash', async () => {
+      const deduplicator = await Deduplicator.create<string | number>({
+        hash: toString
+      })
+
+      await deduplicator.add([123])
+
+      expect(await deduplicator.diff(['123'])).toStrictEqual([])
+    })
+
+    it('with shrink', async () => {
+      // TODO
+    })
   })
 
-  it('diff', async () => {
-    const deduplicator = await Deduplicator.create<string>()
+  describe('diff', () => {
+    it('default', async () => {
+      const deduplicator = await Deduplicator.create<string>()
 
-    const result1 = await deduplicator.diff([])
-    const result2 = await deduplicator.diff(['foo'])
-    const result3 = await deduplicator.diff(['foo', 'bar'])
-    const result4 = await deduplicator.diff([])
+      const result1 = await deduplicator.diff([])
+      const result2 = await deduplicator.diff(['foo'])
+      const result3 = await deduplicator.diff(['foo', 'bar'])
+      const result4 = await deduplicator.diff([])
 
-    expect(result1).toStrictEqual([])
-    expect(result2).toStrictEqual(['foo'])
-    expect(result3).toStrictEqual(['bar'])
-    expect(result4).toStrictEqual([])
+      expect(result1).toStrictEqual([])
+      expect(result2).toStrictEqual(['foo'])
+      expect(result3).toStrictEqual(['bar'])
+      expect(result4).toStrictEqual([])
+    })
+
+    it('with custom hash', async () => {
+      const deduplicator = await Deduplicator.create<string | number>({
+        hash: toString
+      })
+
+      const result1 = await deduplicator.diff([123])
+      const result2 = await deduplicator.diff([456])
+
+      expect(result1).toStrictEqual([123])
+      expect(result2).toStrictEqual([456])
+    })
+
+    it('with shrink', async () => {
+      // TODO
+    })
   })
 
-  it('lastDiff', async () => {
-    const deduplicator = await Deduplicator.create<string>()
+  describe('lastDiff', () => {
+    it('default', async () => {
+      const deduplicator = await Deduplicator.create<string>()
 
-    const result1 = await deduplicator.lastDiff('foo')
-    const result2 = await deduplicator.lastDiff('foo')
-    const result3 = await deduplicator.lastDiff('bar')
+      const result1 = await deduplicator.lastDiff('foo')
+      const result2 = await deduplicator.lastDiff('foo')
+      const result3 = await deduplicator.lastDiff('bar')
 
-    expect(result1).toBe(true)
-    expect(result2).toBe(false)
-    expect(result3).toBe(true)
+      expect(result1).toBe(true)
+      expect(result2).toBe(false)
+      expect(result3).toBe(true)
+    })
+
+    it('with custom hash', async () => {
+      const deduplicator = await Deduplicator.create<string | number>({
+        hash: toString
+      })
+
+      const result1 = await deduplicator.lastDiff(123)
+      const result2 = await deduplicator.lastDiff('123')
+      const result3 = await deduplicator.lastDiff(456)
+
+      expect(result1).toBe(true)
+      expect(result2).toBe(false)
+      expect(result3).toBe(true)
+    })
+
+    it('with shrink', () => {
+      // TODO
+    })
   })
 })
